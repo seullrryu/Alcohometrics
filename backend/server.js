b6bf3b7798651f4526632de14342be641c045137
Seulmin Ryu <sr4632@nyu.edu>
2020-04-21 20:21:54 -0400

    changed middleware

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ connection.once("open", () => {
 })
 
 //Some Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
